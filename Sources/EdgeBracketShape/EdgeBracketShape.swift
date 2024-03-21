0153b2a3b78171cbfa339b3be91d1d0f0bc33704
Edon <edon@valdman.works>
2024-03-21 18:35:28 -0500

    Update EdgeBracketShape.swift
    Implemented use of `Origin`.

--- Sources/EdgeBracketShape/EdgeBracketShape.swift
+++ Sources/EdgeBracketShape/EdgeBracketShape.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 /// A customizable bracket `Shape` to be used around the side of another `View`.
 public struct EdgeBracket: Shape {
-    /// The edge to draw the bracket.
-    public let edge: Edge
+    /// Animatable origin of the shape.
+    ///
+    /// This keeps track of which edge to draw the bracket, while calculating animatable points for the shape.
+    public var origin: Origin
     
     /// The length of the bracket "forks".
     public var bracketLength: CGFloat
@@ -25,68 +27,25 @@ public struct EdgeBracket: Shape {
         bracketLength: CGFloat = 10,
         clamped: Bool = true
     ) {
-        self.edge = edge
+        self.origin = .init(edge: edge)
         self.bracketLength = bracketLength
         self.clamped = clamped
     }
     
-    public var animatableData: CGFloat {
-        get { bracketLength }
-        set { bracketLength = newValue }
+    public var animatableData: AnimatablePair<UnitPoint.AnimatableData, CGFloat> {
+        get {
+            return .init(origin.edgeCenter.animatableData, bracketLength)
+        } set {
+            self.origin.edgeCenter.animatableData = newValue.first
+            self.bracketLength = newValue.second
+        }
     }
     
     public func path(in rect: CGRect) -> Path {
         var path = Path()
-        
-        // Properies are named relative to leading edge bracket
-        let edgeStart: CGPoint
-        let edgeEnd: CGPoint
-        var topDashEnd: CGPoint
-        var bottomDashEnd: CGPoint
-        
-        switch edge {
-        case .leading, .trailing:
-            edgeStart = CGPoint(x: rect.minX, y: rect.minY)
-            edgeEnd = CGPoint(x: rect.minX, y: rect.maxY)
-            topDashEnd = CGPoint(x: rect.minX + bracketLength, y: rect.minY)
-            bottomDashEnd = CGPoint(x: rect.minX + bracketLength, y: rect.maxY)
-            
-            if clamped {
-                topDashEnd.x = min(rect.minX + bracketLength, rect.maxX)
-                bottomDashEnd.x = min(rect.minX + bracketLength, rect.maxX)
-            }
-            
-        case .top, .bottom:
-            edgeStart = CGPoint(x: rect.maxX, y: rect.minY)
-            edgeEnd = CGPoint(x: rect.minX, y: rect.minY)
-            topDashEnd = CGPoint(x: rect.maxX, y: rect.minY + bracketLength)
-            bottomDashEnd = CGPoint(x: rect.minX, y: rect.minY + bracketLength)
-            
-            if clamped {
-                topDashEnd.y = min(rect.minY + bracketLength, rect.maxY)
-                bottomDashEnd.y = min(rect.minY + bracketLength, rect.maxY)
-            }
-        }
-        
-        // Dash 1
-        path.move(to: topDashEnd)
-        path.addLine(to: edgeStart)
-        
-        // Edge
-        path.addLine(to: edgeEnd)
-        
-        // Dash 2
-        path.addLine(to: bottomDashEnd)
-        
-        // For bottom and trailing shapes, rotate on center point 180°
-        if [.bottom, .trailing].contains(edge) {
-            return path.applying(
-                .init(rotationAngle: .pi)
-                .translatedBy(x: -rect.width, y: -rect.height)
-            )
-        } else {
-            return path
-        }
+        let points = origin.pathPoints(for: rect, withBracketLength: bracketLength)
+        path.addLines(points)
+        return path
     }
     
     /// Its behaviour is to not flip automatically in right-to-left contexts so it acts consistently.
